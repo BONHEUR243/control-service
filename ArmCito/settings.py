6ee40a8aa7e9458de310bf6d4fd23310d94a5c2b
BONHEUR243 <armandcito@gmail.com>
2024-07-14 11:57:18 +0200

    commit6

--- ArmCito/settings.py
+++ ArmCito/settings.py
@@ -147,7 +147,7 @@ MEDIA_ROOT = BASE_DIR/ 'static/images'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- ArmCito/urls.py
+++ ArmCito/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.conf import settings
 from django.urls import path,include
-from django.conf.urls.static import static
+
 
 
 
@@ -10,6 +9,3 @@ urlpatterns = [
     path('',include('Custom_user.urls')),
     path('',include('students_admins.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
